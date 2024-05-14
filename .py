4aec9ac258896b83df84011de8c6322f5e6a14d0
Izzatbek777 <izzatbeksmoilov777@gmail.com>
2024-05-14 20:23:33 +0500

    Update .py
    versiya2

--- .py
+++ .py
@@ -4,3 +4,14 @@ conn = sqlite3.connect('medicc.db')
 cursor = conn.cursor()
 cursor.execute(
     '''CREATE TABLE IF NOT EXISTS medicc(id INT, name TEXT,bdate INT, price INT)''')
+
+conn = sqlite3.connect('medicc.db')
+cursor = conn.cursor()
+cursor.execute(
+    '''CREATE TABLE IF NOT EXISTS medicc(id INT, name TEXT,bdate INT, price INT)''')
+cursor.execute('''INSERT INTO medicc(id,  name, bdate, price) VALUES  (2, 'sitromon', '6 month', '12000')''')
+cursor.execute('''INSERT INTO medicc(id,  name, bdate, price) VALUES  (6, 'tirmol', '2 years', '8000')''')
+cursor.execute('''INSERT INTO medicc(id,  name, bdate, price) VALUES  (4, 'livomikol', '8 month', '2000')''')
+
+
+conn.commit()
